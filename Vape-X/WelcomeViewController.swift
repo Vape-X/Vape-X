f33234e0452d8eb16333caa5621d6f0f609c776e
anas <uns.muhammad8@gmail.com>
2022-09-22 22:16:04 -0400

    add view controller

---
+++ Vape-X/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  Vape-X
+//
+//  Created by Muhammad Uns on 9/22/22.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func createAccount(_ sender: Any) {
+    }
+    
+    @IBAction func login(_ sender: Any) {
+    }
+}
