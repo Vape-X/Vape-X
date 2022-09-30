23d0411860ef603e17f79a0f848018ebd951938a
Bilal Naeem <iOSbilalnaeem@gmail.com>
2022-09-29 23:26:17 -0400

    [TK-12]: Logic for login

--- Vape-X/LoginViewController.swift
+++ Vape-X/LoginViewController.swift
@@ -14,6 +14,7 @@ class LoginViewController: UIViewController {
     @IBAction func forgotPasswordButton(_ sender: UIButton) {
     }
     @IBAction func loginButton(_ sender: UIButton) {
+        unWrapTextField()
     }
     @IBAction func facebookButton(_ sender: UIButton) {
     }
@@ -26,4 +27,36 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func unWrapTextField() {
+        if let userNameText = txtfieldUsername.text, let passwordText = txtfieldPassword.text {
+            if passwordText.validPassword(){
+                txtfieldPassword.layer.borderColor = UIColor.clear.cgColor
+            } else {
+                txtfieldPassword.layer.borderColor = UIColor.red.cgColor
+                txtfieldPassword.layer.borderWidth = 1.0
+                txtfieldPassword.shake()
+            }
+        }
+    }
+}
+
+extension UITextField {
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = CGPoint(x: self.center.x - 4.0, y: self.center.y)
+        animation.toValue = CGPoint(x: self.center.x + 4.0, y: self.center.y)
+        layer.add(animation, forKey: "position")
+    }
+}
+
+extension String{
+    
+    func validPassword() -> Bool {
+            let passwordRegex = "^(?=.*[!@#$&*])(?=.*[0-9]).{6,}$"
+            return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
+        }
 }
