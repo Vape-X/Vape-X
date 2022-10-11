8c9d7823e6b093fa9ebc1eaf6054f1affe31735d
WaqarUnNisa <>
2022-10-10 22:21:52 -0500

    Resolved issues.

--- Vape-X/TermsAndConditionViewController.swift
+++ Vape-X/TermsAndConditionViewController.swift
@@ -9,15 +9,19 @@ import UIKit
 
 class TermsAndConditionViewController: UIViewController {
     
-    @IBOutlet weak var mybuttonXCenter: NSLayoutConstraint!
-    @IBOutlet weak var myTextView: UITextView!
-    @IBOutlet weak var myTextViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var readMoreButtonXCenter: NSLayoutConstraint!
+    @IBOutlet weak var termsAndConditionTextView: UITextView!
+    @IBOutlet weak var termsAndConditiontextViewHeight: NSLayoutConstraint!
     @IBOutlet weak var readMoreButtonOutlet: UIButton!
     var isLabelMaxHeight = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureReadMoreButton()
+    }
+    
+    @IBAction func goToNextVc(_ sender: Any) {
+        print("go to next VC")
     }
     
     @IBAction func readMoreButton(_ sender: Any) {
@@ -28,20 +32,20 @@ class TermsAndConditionViewController: UIViewController {
         if isLabelMaxHeight {
             readMoreButtonOutlet.setTitle("", for: .normal)
             isLabelMaxHeight = false
-            myTextViewHeight.constant = 280
+            termsAndConditiontextViewHeight.constant = 280
             
             readMoreButtonOutlet.setImage(UIImage(systemName: "chevron.up")?.withTintColor(.systemBrown, renderingMode: .alwaysOriginal), for: .normal)
-            mybuttonXCenter.constant = 80
+            readMoreButtonXCenter.constant = 80
             readMoreButtonOutlet.backgroundColor = .clear
-            myTextView.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ghefn jhuhfe espam sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ghefn.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+            termsAndConditionTextView.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ghefn jhuhfe espam sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ghefn.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
         } else {
             isLabelMaxHeight = true
             readMoreButtonOutlet.setTitle("Read More", for: .normal)
             readMoreButtonOutlet.backgroundColor = .systemBrown
-            myTextViewHeight.constant =  241
-            mybuttonXCenter.constant = 0
+            termsAndConditiontextViewHeight.constant =  241
+            readMoreButtonXCenter.constant = 0
             readMoreButtonOutlet.setImage(UIImage(systemName: "chevron.down"), for: .normal)
-            myTextView.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ghefn jhuhfe espam sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ghefn."
+            termsAndConditionTextView.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ghefn jhuhfe espam sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ghefn."
         }
     }
 }
