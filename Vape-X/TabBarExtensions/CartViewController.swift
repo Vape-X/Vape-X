fc7d166adbaf08d726d8c6716ea12b7e9a33c45f
Bilal Naeem <iOSbilalnaeem@gmail.com>
2022-10-16 23:25:49 -0400

    feature/TK-25-create-tab-bar

---
+++ Vape-X/TabBarExtensions/CartViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CartViewController.swift
+//  Vape-X
+//
+//  Created by Bilal Naeem on 10/16/22.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
